8868583690d39d9b19b127c4871cd55abc6ae99f
vighnesh153 <vighnesh.raut13@gmail.com>
2023-10-01 22:43:09 +0530

    break down kotlinRun commands into multiple lines

--- common-config.sh
+++ common-config.sh
@@ -9,8 +9,8 @@ nvm use 20
 
 # Run kotlin file with main function: "kotlinRun hello.kt"
 function kotlinRun {
-  TEMP_OUTPUT_FILE="$(xxd -l16 -ps /dev/urandom).jar"
-  kotlinc $1 -include-runtime -d $TEMP_OUTPUT_FILE && \
-    java -jar $TEMP_OUTPUT_FILE && \
-        rm -rf $TEMP_OUTPUT_FILE
+    TEMP_OUTPUT_FILE="$(xxd -l16 -ps /dev/urandom).jar"
+    kotlinc $1 -include-runtime -d $TEMP_OUTPUT_FILE
+    java -jar $TEMP_OUTPUT_FILE
+    rm -rf $TEMP_OUTPUT_FILE
 }
