9733a0c9aadb2b8dedd96b47b78c70d16d9ff70c
vighnesh153 <vighnesh.raut13@gmail.com>
2023-10-03 08:25:46 +0530

    add tmp directory to kotlinRun

--- common-config.sh
+++ common-config.sh
@@ -9,7 +9,9 @@ nvm use 20
 
 # Run kotlin file with main function: "kotlinRun hello.kt"
 function kotlinRun {
-    TEMP_OUTPUT_FILE="$(xxd -l16 -ps /dev/urandom).jar"
+    TMP_DIR=".kotlinRunTmp"
+    TEMP_OUTPUT_FILE="$TMP_DIR/$(xxd -l16 -ps /dev/urandom).jar"
+    mkdir -p $TMP_DIR
     kotlinc $1 -include-runtime -d $TEMP_OUTPUT_FILE
     java -jar $TEMP_OUTPUT_FILE
     rm -rf $TEMP_OUTPUT_FILE
