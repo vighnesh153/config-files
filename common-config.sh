dac9acd25e0af6af140f96255319ea77217ddaf7
vighnesh153 <vighnesh.raut13@gmail.com>
2023-12-28 10:39:31 +0530

    remove "nvm use" from common config

--- common-config.sh
+++ common-config.sh
@@ -4,8 +4,8 @@
 git config --global alias.adog "log --all --decorate --oneline --graph"
 alias gadog="git adog"
 
-# Use node 20
-nvm use 20
+# Use node 20 (too slow)
+# nvm use 20
 
 # Run kotlin file with main function: "kotlinRun hello.kt"
 function kotlinRun {
